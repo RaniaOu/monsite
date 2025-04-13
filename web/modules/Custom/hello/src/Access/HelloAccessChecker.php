6ee6620a6041b75dc5524779878d42bfad178b60
Rania Ounaggou <rania.ounaggou96@gmail.com>
2025-04-13 15:54:22 +0100

    access check rectif code

--- web/modules/Custom/hello/src/Access/HelloAccessChecker.php
+++ web/modules/Custom/hello/src/Access/HelloAccessChecker.php
@@ -7,73 +7,79 @@ namespace Drupal\hello\Access;
 use DateTime;
 use DateTimeInterface;
 use Drupal\Component\Datetime\Time;
+
+
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\Routing\Route;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Datetime\TimeInterface;
-
+use Drupal\Component\Datetime\TimeInterface;
 
 
 /**
- * Checks if passed parameter matches the route configuration.
- *
- * Usage example:
- * @code
- * foo.example:
- *   path: '/example/{parameter}'
- *   defaults:
- *     _title: 'Example'
- *     _controller: '\Drupal\hello\Controller\HelloController'
- *   requirements:
- *     _hello: 'some value'
- * @endcode
+ * Checks access based on user account age and custom route requirement.
  */
 final class HelloAccessChecker implements AccessInterface {
 
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, Time $time)
-  {
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Core\Datetime\TimeInterface
+   */
+  protected TimeInterface $time;
+
+  /**
+   * Constructs a new HelloAccessChecker.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Datetime\TimeInterface $time
+   *   The time service.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, TimeInterface $time)
+{
     $this->entityTypeManager = $entityTypeManager;
     $this->time = $time;
-  }
+}
 
 
   /**
-   * Access callback.
+   * Access callback to check if user account is older than required hours.
    *
-   * @DCG
-   * Drupal does some magic when resolving arguments for this callback. Make
-   * sure the parameter name matches the name of the placeholder defined in the
-   * route, and it is of the same type.
-   * The following additional parameters are resolved automatically.
-   *   - \Drupal\Core\Routing\RouteMatchInterface
-   *   - \Drupal\Core\Session\AccountInterface
-   *   - \Symfony\Component\HttpFoundation\Request
-   *   - \Symfony\Component\Routing\Route
+   * @param \Symfony\Component\Routing\Route $route
+   *   The route object.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The current user account.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
    */
-
-
-   public function access(Route $route, AccountInterface $account): AccessResult {
-
-    if($account->isAnonymous()){
+  public function access(Route $route, AccountInterface $account): AccessResult {
+    if ($account->isAnonymous()) {
       return AccessResult::forbidden();
     }
-    $nbr_heures= $route->getRequirement('_hello');
 
-   /** @var \Drupal\user\Entity\user $user */
+    $nbr_heures = (int) $route->getRequirement('_hello');
 
-   $user = \Drupal::entityTypeManager()->getStorage('user')->load($account->id());
+    /** @var \Drupal\user\Entity\User $user */
+    $user = $this->entityTypeManager->getStorage('user')->load($account->id());
 
-   $created= $user->getCreatedTime();
-   $now =\Drupal::time()->getRequestTime();
+    if (!$user) {
+      return AccessResult::forbidden();
+    }
 
-   return AccessResult::allowedif($now - $created > $nbr_heures * 3600);
+    $created = $user->getCreatedTime();
+    $now = $this->time->getRequestTime();
 
-
-
-
-}
-
+    return AccessResult::allowedIf(($now - $created) > $nbr_heures * 3600);
+  }
 
 }
